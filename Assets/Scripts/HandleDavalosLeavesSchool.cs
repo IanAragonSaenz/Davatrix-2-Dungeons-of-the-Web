0c11c7ea34065f90feb16831c46896f926ca6984
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-04-12 03:58:55 -0500

    first scene is completely done

---
+++ Assets/Scripts/HandleDavalosLeavesSchool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleDavalosLeavesSchool : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    AudioSource audio;
+    SpriteRenderer blackScreen;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        audio = GameObject.Find("first_scene_song").GetComponent<AudioSource>();
+        blackScreen = GameObject.Find("blackout").GetComponent<SpriteRenderer>();
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        double newVolume = (audio.volume) - 0.0005;
+        audio.volume = float.Parse(newVolume.ToString());
+        blackScreen.color += new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, blackScreen.color.a + 0.1f);
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
