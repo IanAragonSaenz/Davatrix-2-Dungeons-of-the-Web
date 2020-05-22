643837636e4fe710cdd8d5b616ad3ad7cfee37ce
marcos <a00824603@itesm.mx>
2020-05-22 13:57:37 -0600

    davalos -> animacion movimiento, dispara al mouse, balas con particulas al chocar

---
+++ Assets/PlayerBulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletScript : MonoBehaviour
+{
+
+    public GameObject bulletParticles;
+
+    public float damage = 20f;
+    
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            // falta poner que la bala haga daño al enemigo
+            Instantiate(bulletParticles, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
